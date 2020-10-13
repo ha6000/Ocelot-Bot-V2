867f66f566ae09483c3ce34856e73e4295fa9faa
Harry <krugerh27@gmail.com>
2020-10-13 23:31:45 +0200

    Fixing undefined name

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ async function updateMember(roles, member) {
 		console.error(err);
 		return log(new discord.MessageEmbed({
 			title: 'User skipped',
-			description: `**${member.user.displayName}** was skipped because **${err.message}** error occured`
+			description: `**${member.displayName}** was skipped because **${err.message}** error occured`
 		}).setColor('BLUE'));
 	}
 
